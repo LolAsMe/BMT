bc43dff6dcb8a821df8693313e69bf9e2e973ee0
LolAsMe <stared@ymail.com>
2023-04-11 00:36:36 +0700

    check point

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kas;
+use App\Models\Simpanan;
+use App\Models\Transaksi;
 use App\Models\TransaksiHarian;
 use Auth;
 use Illuminate\Http\Request;
@@ -17,9 +20,25 @@ class DashboardController extends Controller
             $group->load('anggota:id,nama');
             return Inertia::render('DashboardKaryawan', compact('group'));
         } else if (Auth::user()->hasJabatan('Manajer') || Auth::user()->hasJabatan('Teller')) {
-            $transaksiHarian = TransaksiHarian::whereDate('tanggal',now()->format('Y-m-d'))->get();
+            $kasBMT = Kas::find(2);
+            $kasBrankas = Kas::find(1);
+            $tanggal = now();
 
-            return Inertia::render('Dashboard',compact('transaksiHarian'));
+            $transaksis = Transaksi::whereDate('tanggal_transaksi', $tanggal)->with(['log'])->get()->loadMorph('log', [
+                DetailSimpanan::class => ['simpanan.anggota'],
+                DetailPembiayaan::class => ['pembiayaan.anggota'],
+            ]);
+            $attribute = [];
+            $attribute['jumlahDebit'] =  $transaksis->sum('debit');
+            $attribute['jumlahKredit'] =  $transaksis->sum('kredit');
+            $attribute['saldoAwal'] =  $kasBMT->detail()->whereDate('tanggal', now())->first()->saldo_awal;
+            $attribute['selisih'] =  $transaksis->sum('debit') - $transaksis->sum('kredit');
+            return Inertia::render('Dashboard', [
+                'kasBMT' => $kasBMT,
+                'kasBrankas' => $kasBrankas,
+                'transaksis' => $transaksis,
+                'attribute' => $attribute,
+            ]);
         };
     }
 }
